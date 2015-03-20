8dd1fa38293c37bc4bc75c11bae98889c64a3136
Sean Clarke <sean@nay-clarke.com>
2015-03-20 21:36:13 +0000

    Work in progress - saving for now :-)

--- CodeBook.md
+++ CodeBook.md
@@ -6,12 +6,27 @@ activity_labels.txt - maps numeric codes to quantities (eg 1 -> Walking)
 
 features_info.txt - summary of features
 
-features.txt - mapping of numerical codes to features description (eg 1 -> tBodyAcc-mean()-X)
+features.txt (561 obs x 2 vars) - mapping of numerical codes to features description (eg 1 -> tBodyAcc-mean()-X)
 
 test/Inertial signals - accelerometer data
 
-test/subject_test.txt - observation labels
+test/subject_test.txt (2947 obs x 1 var) - observation labels
 
-test/y_test.txt  - feature labels
+test/y_test.txti (2947 obs x 1 var  - feature labels
 
-test/X_test.txt - data
+test/X_test.txt  (2947 obs x 561 var)- data
+
+train/Inertial signals - accelerometer data
+
+train/subject_test.txt (7532 obs x 1 var) - observation labels
+
+train/y_test.txti (7532 obs x 1 var  - feature labels
+
+train/X_test.txt  (7532 obs x 561 var)- data
+
+<h2> Initial data transformations </h2>
+<ol> cbind the subject_T[rain|est] and y_t[rain|est] vectors onto X_t[rain|est] - this links the subjects and features to the observations.a
+<ol> Use features.txt to create variable headers
+<ol> rbind the training and testing data sets together as per the spec
+<ol> Discard data columns that dont have mean or std in the name - done using grepl - dplyr would have been nicer but non-standard var names make it think that there are duplicate names.
+</ol>
---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(dplyr)
+
+#### Initially we read the data in
+
+
+features <- read.table("UCI HAR Dataset/features.txt") ### This file stores headers
+
+## test data
+
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")  ### test data vector
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names="subject")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names="feature")
+
+#### Read in features as the variable headers for X_test
+names(X_test) <- features[,2]
+
+#### cbind subject_test and y_test to X_test to give labels of observation details.
+X_test <- cbind(subject_test, y_test, X_test)
+
+## train data
+
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")  ### train data vector
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names="subject")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names="feature")
+
+#### Read in features as the variable headers for X_train
+names(X_train) <- features[,2]
+
+#### cbind subject_test and y_test to X_test to give labels of observation details.
+X_train <- cbind(subject_train, y_train, X_train)
+
+### rbind test and training data to give a consolidated data set
+
+df <- rbind(X_test, X_train)
+
+### Extract columns containing Means and SDs using grepl
+### Wanted to use dplyr but the non-standard characters
+### in the column names broke that!
+
+df <-  df[grepl("mean|std|subject|feature", names(df))]
